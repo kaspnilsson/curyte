74d5039d93cba5cfed2a0f696f5c7a6a5567b126
Kasper Nilsson <kaspern@google.com>
2021-11-06 13:45:15 -0700

    Add typings for modules so deploy doesnt break

---
+++ curyte-ui/typings.d.ts
@@ -0,0 +1,7 @@
+declare module '@material-tailwind/react/Image';
+declare module '@material-tailwind/react/Button';
+declare module '@material-tailwind/react/Dropdown';
+declare module '@material-tailwind/react/DropdownItem';
+declare module '@material-tailwind/react/Card';
+declare module '@material-tailwind/react/CardHeader';
+declare module '@material-tailwind/react/CardBody';
