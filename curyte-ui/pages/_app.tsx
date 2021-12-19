66688882da74e72314213b98c7faf0704f49bae3
Kasper Nilsson <kaspern@google.com>
2021-12-18 21:03:19 -0800

    Remove annoying focus border from textareas

--- curyte-ui/pages/_app.tsx
+++ curyte-ui/pages/_app.tsx
@@ -1,8 +1,8 @@
 import { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import 'remixicon/fonts/remixicon.css'
-import '../styles/app.scss'
 import '../styles/index.css'
+import '../styles/app.scss'
 import { useRouter } from 'next/router'
 import { pageview } from '../utils/gtag'
 import { useEffect } from 'react'
@@ -13,12 +13,8 @@ export default function MyApp({ Component, pageProps }: AppProps) {
     const handleRouteChange = (url: URL) => {
       pageview(url)
     }
-    //When the component is mounted, subscribe to router changes
-    //and log those page views
-    router.events.on('routeChangeComplete', handleRouteChange)
 
-    // If the component is unmounted, unsubscribe
-    // from the event with the `off` method
+    router.events.on('routeChangeComplete', handleRouteChange)
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
