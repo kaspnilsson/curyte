86dc9802082c8f69f2a122631eac9548325ea4bc
Kasper Nilsson <kaspern@google.com>
2021-12-18 20:36:10 -0800

    Add analytics

---
+++ curyte-ui/utils/gtag.ts
@@ -0,0 +1,32 @@
+export const GA_TRACKING_ID = 'G-DZN4F4RQ37'
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/pages
+export const pageview = (url: URL) => {
+  if (process.env.NODE_ENV !== 'production') {
+    window.gtag('config', GA_TRACKING_ID, {
+      page_path: url,
+    })
+  } else {
+    console.log(`would have logged pageview: ${url}`)
+  }
+}
+
+type GTagEvent = {
+  action: string
+  category: string
+  label: string
+  value: number
+}
+
+// https://developers.google.com/analytics/devguides/collection/gtagjs/events
+export const event = (event: GTagEvent) => {
+  if (process.env.NODE_ENV !== 'production') {
+    window.gtag('event', event.action, {
+      event_category: event.category,
+      event_label: event.label,
+      value: event.value,
+    })
+  } else {
+    console.log(`would have logged event: ${event}`)
+  }
+}
