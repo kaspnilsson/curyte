627ccc821dba363b2b3b98ff46908f3c886f9320
Kasper Nilsson <kaspern@google.com>
2021-12-20 14:57:24 -0800

    Build & serve sitemap so google crawls us

---
+++ curyte-ui/next-sitemap.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteUrl: 'https://www.curyte.com',
+  generateRobotsTxt: true,
+  exclude: ['/server-sitemap.xml'], // <= exclude here
+  robotsTxtOptions: {
+    additionalSitemaps: [
+      'https://www.curyte.com/server-sitemap.xml', // <==== Add here
+    ],
+  },
+}
--- curyte-ui/next.config.js
+++ curyte-ui/next.config.js
@@ -1,7 +1,12 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 /** @type {import('next').NextConfig} */
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+})
+
+module.exports = withBundleAnalyzer({
   reactStrictMode: true,
   images: {
     domains: ['firebasestorage.googleapis.com'],
   },
-}
+})
