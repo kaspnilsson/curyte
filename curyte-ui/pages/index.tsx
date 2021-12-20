a8f42a951a06f79cc26ba756cdc64289734b01e0
Kasper Nilsson <kaspern@google.com>
2021-12-20 14:14:16 -0800

    Update changes to marketing page as requested

--- curyte-ui/pages/index.tsx
+++ curyte-ui/pages/index.tsx
@@ -29,13 +29,18 @@ const Home = () => {
     <Layout>
       <Container className="flex flex-col items-center">
         <section className="flex-row flex items-center justify-center mt-16 mb-8">
-          <h1 className="text-8xl text-center font-bold tracking-tighter leading-tight flex items-center gap-4 aspect-[3/1]">
-            <CuryteLogo height={'97.5px'} width={'97.5px'} />
+          <h1 className="text-6xl md:text-8xl text-center font-bold tracking-tighter leading-tight flex items-center gap-2 md:gap-4 aspect-[3/1]">
+            <div className="hidden md:flex h-min-content">
+              <CuryteLogo height={'97.5px'} width={'97.5px'} />
+            </div>
+            <div className="flex md:hidden h-min-content">
+              <CuryteLogo height={'71px'} width={'71px'} />
+            </div>
             Curyte
           </h1>
         </section>
         <section className="flex-col flex items-center justify-around mb-8">
-          <div className="aspect-video shadow-xl shadow-purple-500/20 rounded-xl overflow-hidden mb-16 border-2 border-slate-200 lg:max-w-[60vw]">
+          <div className="shadow-xl shadow-purple-500/20 rounded-xl overflow-hidden mb-16 border-2 border-slate-200 lg:max-w-[60vw]">
             <video autoPlay loop muted>
               <source src="/static/promo.webm" />
             </video>
@@ -44,7 +49,7 @@ const Home = () => {
             Engaging lessons,
             <div className="text-purple-500">digitally native.</div>
           </div>
-          <div className="mb-8 flex items-center align-center gap-4">
+          <div className="mb-8 flex flex-col md:flex-row items-center align-center gap-4">
             <Link passHref href={lessonSearchRoute()}>
               <Button
                 className="flex-1 flex"
@@ -65,51 +70,79 @@ const Home = () => {
           </div>
         </section>
         <section className="flex-col flex items-center justify-around mb-8">
-          <p className="text-3xl tracking-tighter leading-tight font-semibold">
+          <p className="text-2xl md:text-3xl tracking-tighter leading-tight font-semibold">
             Curyte is a new lesson builder & library for the remote age. Collect
             and organize docs, share them with students and colleagues, and
             explore what everyone else is teaching.
           </p>
         </section>
-        <section className="columns-sm text-xl w-full mb-32">
-          <ul>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Search through free lessons
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Save a library of lessons
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Copy and modify lessons
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Collect donations
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Google Docs integration
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Youtube integration
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Rich text formatting
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Embed any web page
-            </li>
-            <li className="py-2 flex gap-3 items-center">
-              <div className="text-base">✅</div>
-              Interactive quizzes
-            </li>
-          </ul>
+        <section className="w-full mb-16 md:mb-32 flex justify-around flex-wrap flex-col lg:flex-row gap-8">
+          <div className="flex flex-col items-center flex-1 text-base md:text-xl">
+            <ul className="flex-1">
+              <h3 className="text-2xl mb-4 tracking-tighter leading-tight font-bold text-center">
+                Browse
+              </h3>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Search through free lessons
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Explore what others are teaching
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Collect a library of lessons
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Build your personal profile
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Send lessons to learners with just a link
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Collect donations
+              </li>
+            </ul>
+          </div>
+          <div className="flex flex-col items-center flex-1">
+            <ul className="flex-1">
+              <h3 className="text-2xl mb-4 tracking-tighter leading-tight font-bold text-center">
+                Build
+              </h3>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Create interactive lessons
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Add questions, videos, and entire websites
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Insert Google Docs, Slides, and more
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Insert Youtube videos
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Tag with topics, standards, and skill level
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Organize lessons into paths
+              </li>
+              <li className="py-2 flex gap-3 items-center">
+                <div className="text-base">✅</div>
+                Share your lessons with the world
+              </li>
+            </ul>
+          </div>
         </section>
         <section className="flex-col flex items-center justify-around mb-8">
           <div className="text-center mb-8 text-4xl font-bold tracking-tighter leading-tight">
