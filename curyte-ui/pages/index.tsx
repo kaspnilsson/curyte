b68b82e5471bd02eef3ba8cfd0eca4519ea5764e
Kasper Nilsson <kaspern@google.com>
2021-12-20 16:34:52 -0800

    Try it with mp4 instead because wtf man

--- curyte-ui/pages/index.tsx
+++ curyte-ui/pages/index.tsx
@@ -42,7 +42,7 @@ const Home = () => {
         <section className="flex-col flex items-center justify-around mb-8">
           <div className="shadow-xl shadow-purple-500/20 rounded-xl overflow-hidden mb-16 border-2 border-slate-200 lg:max-w-[60vw]">
             <video autoPlay={true} loop={true} muted={true} playsInline={true}>
-              <source src="/static/promo.webm" type="video/webm" />
+              <source src="/static/promo.mp4" type="video/mp4" />
             </video>
           </div>
           <div className="text-center mb-8 text-4xl font-bold tracking-tighter leading-tight">
