fbd14d0a94ad7c56f410432f3a6e7afd1e5d1304
Kasper Nilsson <kaspern@google.com>
2021-12-22 15:31:42 -0800

    Add delete button to draft list

--- curyte-ui/components/DraftLink.tsx
+++ curyte-ui/components/DraftLink.tsx
@@ -20,9 +20,9 @@ const DraftLink = ({ draft }: Props) => {
       <Button
         variant="link"
         colorScheme="black"
-        className="overflow-hidden rounded hover:bg-zinc-200"
+        className="overflow-hidden rounded hover:bg-zinc-100"
       >
-        <span className="flex items-center w-auto p-1 m-0 leading-tight tracking-tight">
+        <span className="flex items-center w-auto p-1 pr-2 m-0 leading-tight tracking-tight">
           <DocumentTextIcon className="w-5 h-5 mr-1" />
           {draft.title || '(no title)'}
         </span>
--- curyte-ui/pages/drafts/all.tsx
+++ curyte-ui/pages/drafts/all.tsx
@@ -4,9 +4,12 @@ import { Lesson } from '../../interfaces/lesson'
 import LoadingSpinner from '../../components/LoadingSpinner'
 import { auth } from '../../firebase/clientApp'
 import DraftLink from '../../components/DraftLink'
-import { getDrafts } from '../../firebase/api'
+import { deleteDraft, getDrafts } from '../../firebase/api'
+import { Tooltip, IconButton, useToast } from '@chakra-ui/react'
+import { TrashIcon } from '@heroicons/react/outline'
 
 const DraftsPage = () => {
+  const toast = useToast()
   const [user, userLoading] = useAuthState(auth)
   const [drafts, setDrafts] = useState<Lesson[]>([])
   const [loading, setLoading] = useState(userLoading)
@@ -23,14 +26,43 @@ const DraftsPage = () => {
     )
   }, [user])
 
+  const localDeleteDraft = async (uid: string) => {
+    setLoading(true)
+    try {
+      await deleteDraft(uid)
+
+      setDrafts(drafts.filter((d) => d.uid !== uid))
+      toast({ title: 'Draft deleted!', status: 'success' })
+    } catch (e) {
+      toast({
+        title: 'Failed to delete draft....',
+        description: e as string,
+        status: 'error',
+      })
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <>
       {loading && <LoadingSpinner />}
       {!loading && (
         <>
           {drafts.map((draft) => (
-            <div className="mb-1" key={draft.uid}>
+            <div className="flex items-center mb-1 group" key={draft.uid}>
               <DraftLink draft={draft} />
+              <Tooltip label="Delete draft">
+                <IconButton
+                  aria-label="Delete draft"
+                  variant="ghost"
+                  className="invisible group-hover:visible"
+                  title="Delete draft"
+                  size="sm"
+                  onClick={() => localDeleteDraft(draft.uid)}
+                  icon={<TrashIcon className="w-5 h-5 text-zinc-900" />}
+                />
+              </Tooltip>
             </div>
           ))}
           {!drafts.length && 'No drafts!'}
