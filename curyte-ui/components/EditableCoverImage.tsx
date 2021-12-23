23af724aed41c5045bbfbf2250e673990a5a09ac
Kasper Nilsson <kaspern@google.com>
2021-12-22 16:43:56 -0800

    Fix 'add cover' btn

--- curyte-ui/components/EditableCoverImage.tsx
+++ curyte-ui/components/EditableCoverImage.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@chakra-ui/button'
+import classNames from 'classnames'
 import React, { useRef } from 'react'
 import useImageUploadDialog from '../hooks/useImageUploadDialog'
 import CoverImage from './CoverImage'
@@ -22,20 +23,21 @@ const EditableCoverImage = ({ title, src, onEditUrl }: Props) => {
     <div className="relative flex items-center justify-center w-full mt-4 sm:mx-0">
       {src && <CoverImage title={title} src={src} />}
       {!src && (
-        <div className="flex items-center justify-center w-full h-32 border-2 border-zinc-200 rounded-xl">
-          No cover image
-        </div>
+        <div className="flex items-center justify-center w-full h-32 border-2 border-zinc-200 rounded-xl"></div>
       )}
       <div
         className="absolute top-0 left-0 z-10 flex w-full h-full cursor-pointer group"
         onClick={() => buttonRef?.current?.click()}
       >
         <Button
-          className="invisible m-auto group-hover:visible"
+          className={classNames('m-auto', {
+            'invisible group-hover:visible': !!src,
+          })}
           ref={buttonRef}
           onClick={onEditImage}
         >
-          Upload new image
+          {!src && 'Add cover'}
+          {!!src && 'Change cover'}
         </Button>
       </div>
     </div>
