2903411a23854cdc5f4839d6e04ef2f30c6111b9
Kasper Nilsson <kaspern@google.com>
2022-01-09 16:17:37 -0800

    Add confirmation before deleting path and lesson

--- curyte-ui/components/LessonHeader.tsx
+++ curyte-ui/components/LessonHeader.tsx
@@ -38,6 +38,7 @@ import {
   removeSavedLessonForCurrentUser,
   saveLessonForCurrentUser,
 } from '../firebase/api'
+import useConfirmDialog from '../hooks/useConfirmDialog'
 
 type Props = {
   lesson: Lesson
@@ -105,8 +106,16 @@ const LessonHeader = ({
     router.push(newLessonRoute(lesson.uid))
   }
 
+  const { ConfirmDialog, onOpen } = useConfirmDialog({
+    title: 'Delete lesson',
+    body: 'Are you sure you want to delete this lesson?',
+    confirmText: 'Delete lesson',
+    onConfirmClick: handleDelete || (() => null),
+  })
+
   return (
     <>
+      <ConfirmDialog />
       <LessonTitle title={lesson?.title || '(no title)'} />
       <div className="flex items-center mb-4 h-min">
         {parentLesson && (
@@ -219,7 +228,7 @@ const LessonHeader = ({
                   </MenuItem>
                 )}
                 {handleDelete && (
-                  <MenuItem onClick={handleDelete}>
+                  <MenuItem onClick={onOpen}>
                     <DocumentRemoveIcon className="w-5 h-5 mr-4 text-inherit" />
                     Delete lesson
                   </MenuItem>
