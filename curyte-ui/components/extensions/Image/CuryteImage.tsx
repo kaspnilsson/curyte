7fbd573d15a9271fbe29b0f9f5f37190f00e46a0
Kasper Nilsson <kaspnilsson@gmail.com>
2022-01-25 17:19:14 -0800

    Update CuryteImage.tsx

--- curyte-ui/components/extensions/Image/CuryteImage.tsx
+++ curyte-ui/components/extensions/Image/CuryteImage.tsx
@@ -25,7 +25,7 @@ const CuryteImage = (uploadFn: UploadFn) => {
     name: 'image',
     addOptions() {
       return {
-        inline: true,
+        inline: false,
         HTMLAttributes: {
           class: 'image-wrapper w-auto h-full min-h-96 shadow-lg rounded-xl',
         },
