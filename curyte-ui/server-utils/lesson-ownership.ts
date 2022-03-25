91c4e111543865d73608a312508f45c6b633154d
Kasper Nilsson <kaspern@google.com>
2022-03-25 11:11:39 -0700

    Allow marvin and kasper to edit other peoples' lessons and paths

--- curyte-ui/server-utils/lesson-ownership.ts
+++ curyte-ui/server-utils/lesson-ownership.ts
@@ -1,9 +1,12 @@
 import prismaClient from '../lib/prisma'
+import { userIsAdmin } from '../utils/hacks'
 
 export const userCanEditLesson = async (
   lessonUid: string,
   authorId: string
 ): Promise<boolean> => {
+  if (userIsAdmin(authorId)) return true
+
   const lesson = await prismaClient.lesson.findFirst({
     where: { uid: lessonUid },
   })
@@ -16,6 +19,8 @@ export const userCanEditPath = async (
   pathUid: string,
   authorId: string
 ): Promise<boolean> => {
+  if (userIsAdmin(authorId)) return true
+
   const path = await prismaClient.path.findFirst({
     where: { uid: pathUid },
   })
