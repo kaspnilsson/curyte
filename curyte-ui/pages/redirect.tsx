78fbad0497bf6a65f775e4ca949671c8509dcc0e
Kasper Nilsson <kaspern@google.com>
2022-05-12 16:28:00 -0700

    Add error handling to redirect page

--- curyte-ui/pages/redirect.tsx
+++ curyte-ui/pages/redirect.tsx
@@ -2,15 +2,24 @@ import React, { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import LoadingSpinner from '../components/LoadingSpinner'
 import { useUserAndProfile } from '../contexts/user'
-import { nextStepsRoute, workspaceRoute } from '../utils/routes'
+import { loginRoute, nextStepsRoute, workspaceRoute } from '../utils/routes'
 
-const NewPathView = () => {
+const Redirect = () => {
   const router = useRouter()
   const { referrer } = router.query
-  const { userAndProfile, loading } = useUserAndProfile()
+  const { userAndProfile, loading, logout } = useUserAndProfile()
 
   useEffect(() => {
-    if (loading || !userAndProfile) return
+    if (loading) return
+    if (!userAndProfile) {
+      console.log('not logged in')
+      if (logout) {
+        logout()
+        console.log('logout fn found, logging out')
+      }
+      router.push(loginRoute())
+      return
+    }
     if (userAndProfile?.profile?.displayName) {
       // If user has name, they probably went thru the walkthrough
       router.push(referrer ? (referrer as string) : workspaceRoute)
@@ -18,9 +27,9 @@ const NewPathView = () => {
       // Drop referrer if user is new.
       router.push(nextStepsRoute())
     }
-  }, [loading, referrer, router, userAndProfile])
+  }, [loading, logout, referrer, router, userAndProfile])
 
   return <LoadingSpinner />
 }
 
-export default NewPathView
+export default Redirect
