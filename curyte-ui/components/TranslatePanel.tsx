9b02dd72adce8198c17c346887064762d08e5cf0
Kasper Nilsson <kaspern@google.com>
2022-05-18 19:20:54 -0700

    Add audio to translation too

--- curyte-ui/components/TranslatePanel.tsx
+++ curyte-ui/components/TranslatePanel.tsx
@@ -1,8 +1,13 @@
-import { IconButton, Select, Spinner } from '@chakra-ui/react'
+import { Button, IconButton, Select, Spinner } from '@chakra-ui/react'
 import { XIcon } from '@heroicons/react/outline'
 import React, { useEffect, useState } from 'react'
 import { useLocalStorage } from 'usehooks-ts'
 import { translate } from '../lib/apiHelpers'
+// No types available for react-speech-kit
+// eslint-disable-next-line
+// @ts-ignore
+import { useSpeechSynthesis } from 'react-speech-kit'
+import { isServerSideRendering } from '../hooks/useWindowSize'
 
 const LANGUAGES = {
   es: 'Spanish',
@@ -11,6 +16,16 @@ const LANGUAGES = {
   am: 'Amharic',
   'zh-CN': 'Chinese',
   vi: 'Vietnamese',
+}
+
+const canNarrate = (lang: string) => {
+  if (isServerSideRendering) return false
+  return !!getVoice(lang)
+}
+
+const getVoice = (lang: string) => {
+  const voices = window.speechSynthesis.getVoices()
+  return voices.find((v) => v.lang.startsWith(lang))
 }
 
 interface Props {
@@ -25,6 +40,7 @@ const TranslatePanel = ({ text, onClose }: Props) => {
   )
   const [loading, setLoading] = useState(true)
   const [translated, setTranslated] = useState('')
+  const { speak, speaking, cancel } = useSpeechSynthesis()
 
   useEffect(() => {
     if (!text || !lang) return
@@ -64,7 +80,7 @@ const TranslatePanel = ({ text, onClose }: Props) => {
           <XIcon className="w-5 h-5 text-zinc-500" />
         </IconButton>
       </div>
-      <div className="p-2 border rounded-xl bg-zinc-50">{text}</div>
+      <div className="px-4 py-2 border rounded-xl bg-zinc-50">{text}</div>
       <Select
         placeholder="Choose a language"
         value={lang}
@@ -80,12 +96,38 @@ const TranslatePanel = ({ text, onClose }: Props) => {
       </Select>
       {loading && <Spinner />}
       {!loading && (
-        <div className="p-2 border rounded-xl bg-zinc-50">
-          {translated && translated}
-          {!translated && (
-            <span className="text-red-500">No translation found!</span>
+        <>
+          <div className="px-4 py-2 border rounded-xl bg-zinc-50">
+            {translated && translated}
+            {!translated && (
+              <span className="text-red-500">No translation found!</span>
+            )}
+          </div>
+          {canNarrate(lang) && (
+            <Button
+              variant={speaking ? 'solid' : 'ghost'}
+              colorScheme="black"
+              onClick={() => {
+                speaking
+                  ? cancel()
+                  : speak({ text: translated, voice: getVoice(lang) })
+              }}
+              className="flex items-center min-w-0 gap-2 !w-fit"
+            >
+              {speaking ? (
+                <>
+                  {'Stop '}
+                  <i className="text-lg font-thin ri-volume-mute-fill"></i>
+                </>
+              ) : (
+                <>
+                  {'Hear it '}
+                  <i className="text-lg font-thin ri-volume-up-fill" />
+                </>
+              )}
+            </Button>
           )}
-        </div>
+        </>
       )}
     </div>
   )
